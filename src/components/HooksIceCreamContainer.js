1983eed38e1f79ba64d05f3644efd6038437de8a
David Arhin <davidarhin2005@gmail.com>
2024-04-29 08:35:23 +0000

    icecream state added

---
+++ src/components/HooksIceCreamContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IceCreamContainer = () => {
+  return (
+    <div>
+        <h3>IceCreamContainer</h3>
+        <button>Buy Ice Cream</button>
+    </div>
+  )
+}
+
+export default IceCreamContainer
